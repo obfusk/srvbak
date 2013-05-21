776d9b86e55b67d6e7a8b061a8b06a105539af0a
Felix C. Stegerman <felixstegerman@noxqslabs.nl>
2013-05-21 17:36:30 +0200

    echo OK

--- srvbak.bash
+++ srvbak.bash
@@ -4,7 +4,7 @@
 #
 # File        : srvbak.bash
 # Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-# Date        : 2013-05-17
+# Date        : 2013-05-21
 #
 # Copyright   : Copyright (C) 2013  Felix C. Stegerman
 # Licence     : GPLv2
@@ -80,6 +80,7 @@ fi
 # 7. after
 run_multi "${after[@]}"
 
+echo OK
 echo
 
 # vim: set tw=70 sw=2 sts=2 et fdm=marker :
